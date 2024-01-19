4d47642f5734dd46ac11ebcd07e5a66b984cf7af
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-01-19 11:36:18 -0500

    fix: update lock file to be consistent with toml file (#99)

--- db_revisions/env.py
+++ db_revisions/env.py
@@ -30,7 +30,10 @@ INST_DB_PWD = os.environ.get("INST_DB_PWD")
 INST_DB_HOST = os.environ.get("INST_DB_HOST")
 INST_DB_NAME = os.environ.get("INST_DB_NAME")
 INST_DB_SCHEMA = os.environ.get("INST_DB_SCHEMA")
-INST_CONN = f"postgresql://{INST_DB_USER}:{(parse.quote(INST_DB_PWD, safe='')).replace('%', '%%')}@{INST_DB_HOST}/{INST_DB_NAME}"
+INST_CONN = (
+    f"postgresql://{INST_DB_USER}:{(parse.quote(INST_DB_PWD, safe='')).replace('%', '%%')}"
+    f"@{INST_DB_HOST}/{INST_DB_NAME}"
+)
 config.set_main_option("sqlalchemy.url", INST_CONN)
 
 # end specific SBL configuration
