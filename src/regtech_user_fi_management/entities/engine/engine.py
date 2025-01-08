fe5bdee2147d6b8a8de2bcad683a8ddf1bfef512
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2025-01-08 10:07:55 -0800

    fix: remove scoped session since fastapi reuse threads, breaking thread local sessions (#283)

--- src/regtech_user_fi_management/entities/engine/engine.py
+++ src/regtech_user_fi_management/entities/engine/engine.py
@@ -1,16 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from regtech_user_fi_management.config import settings
 
 engine = create_engine(str(settings.inst_conn), echo=settings.db_logging).execution_options(
     schema_translate_map={None: settings.inst_db_schema}
 )
-SessionLocal = scoped_session(sessionmaker(engine, expire_on_commit=False))
+SessionLocal = Session = sessionmaker(engine, expire_on_commit=False)
 
 
 def get_session():
-    session = SessionLocal()
-    try:
+    with Session() as session:
         yield session
-    finally:
-        session.close()
